f9b02f491653610cceea89771068232705067e84
Chibuisi Enyia <enyia21@gmail.com>
2020-05-02 04:32:38 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -16,7 +16,7 @@ class Patient
         Appointment.new(date, self, doctor)
     end
 
-    def appointments 
+    def appointments
         Appointment.all.select{|appointment| appointment.patient == self}
     end
 
